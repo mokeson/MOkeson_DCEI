909b16bb9b8416b0680f0e7fdbe9e55014acd681
Morgan Okeson <mokeson@ku.edu>
2018-04-24 10:24:34 -0500

    Help during class time on writing functions, 04/24

---
+++ UnitTesting_HW/PracticeFunctions.R
@@ -0,0 +1,12 @@
+#Function that returns a scatter plot
+#
+#Args
+#x,y two vectors of numbers
+# 
+#Output
+#Plot of x vs. y (x on horizontal axis)
+#No return
+myplot<-function(x,y)
+{
+  plot(y~x)
+}
