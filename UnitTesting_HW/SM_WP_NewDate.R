f71eb206c443892b44da83f9ad60cb9ffbd362c2
Okeson <m898o858@home.ku.edu>
2018-04-24 13:17:12 -0500

    working on new function for changing date and time, have questions about what can be a variable

--- UnitTesting_HW/SM_WP_NewDate.R
+++ UnitTesting_HW/SM_WP_NewDate.R
@@ -2,6 +2,10 @@
 #
 #Args
   #df is the dataframe
+  #
+  #"date_time" is a column in the selected "df", can this be turned into a variable in the function as well?
+  #can the format be turned into a variable?
+#
 #output
   #New date and time format for the "date_time" column
 
@@ -9,10 +13,3 @@ newdate<-function(df)
 {
   return(as.POSIXct(as.character(df[,"Date_Time"]),format="%m/%d/%Y %H:%M"))
 }
-
-
-xlim<-c(as.POSIXct("2017-05-01 01:00:00"),as.POSIXct("2017-07-01 01:00:00"))
-ylim<-c(0.3,0.5)
-plot(FFBS$Date_Time,FFBS$Moist_10cm,type="l",xlim = xlim,ylim = ylim,col='red',xlab="Date",ylab="Soil Moisture (Unit)")
-par(new=T)
-plot(Moist_50cm~Date_Time,FFBS,type="l",xlim = xlim,ylim = ylim,col='blue',xlab="",ylab="")
